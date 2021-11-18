c02ff66cd23f33156cb3065d72baf56108e32083
power_840 <liuyu19921017@gmail.com>
2021-11-18 10:10:53 +0800

    feat: useButton

--- website/App.vue
+++ website/App.vue
@@ -1,4 +1,42 @@
 <template>
-  <z-button icon="bi">按钮</z-button>
+  <z-button
+    icon="bi"
+    :loading="buttonLoading"
+    @click="buttonClick"
+  >按钮</z-button>
   <z-icon name="1" />
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onUnmounted, ref } from "@vue/runtime-core";
+
+function useButton() {
+  const buttonLoading = ref(true)
+  let timer: NodeJS.Timeout
+  onMounted(() => {
+    timer = setTimeout(() => {
+      buttonLoading.value = false
+    }, 2000)
+  })
+  onUnmounted(() => {
+    timer && clearTimeout(timer)
+  })
+
+  const buttonClick = () => {
+    alert('点击按钮')
+  }
+
+  return {
+    buttonLoading,
+    buttonClick
+  }
+
+}
+export default defineComponent({
+  setup() {
+    return {
+      ...useButton()
+    }
+  }
+})
+</script>
